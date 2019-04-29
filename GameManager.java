414f4ea67288d5a99c036a542a0e87273a58afb5
EpicConOwO <48412051+EpicConOwO@users.noreply.github.com>
2019-04-29 14:36:49 -0700

    Update GameManager.java

--- GameManager.java
+++ GameManager.java
@@ -2,9 +2,18 @@ import java.awt.*;
 import javax.swing.*;
 public class GameManager
 {
+	public String OpponentKing;
+	public String PlayerKing;
+	public String PlayerColor;
 	public GameManager()
 	{
 		
+	}
+	public void InitialtizeGame(String P_King,String O_King,String P_color)
+	{
+		PlayerKing=P_King;
+		OpponentKing = O_King;
+		PlayerColor=P_color;
 	}
 	
 	public void NewGame(String Col,int sizex,int sizey,JFrame MaineGameFrame)
@@ -16,5 +25,9 @@ public class GameManager
 	{
 		System.out.println("Exiting GAME");
 		System.exit(0);
+	}
+	public void LogConsole(String log)
+	{
+		System.out.println(log);
 	}
 }
